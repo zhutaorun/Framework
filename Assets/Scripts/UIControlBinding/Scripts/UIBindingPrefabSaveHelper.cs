430af8f51fc2ef55271c718352e2847d8fc71380
zhutaorun <zhutaorun@gmail.com>
2020-10-27 21:23:43 +0800

    优化子节点识别类型型

--- Assets/Scripts/UIControlBinding/Scripts/UIBindingPrefabSaveHelper.cs
+++ Assets/Scripts/UIControlBinding/Scripts/UIBindingPrefabSaveHelper.cs
@@ -43,35 +43,47 @@ namespace SDGame.UITools
         public static void SavePrefab(GameObject goInHierarchy)
         {
             UnityEngine.Object goPrefab = null;
-            while (goPrefab == null)
+            UnityEngine.GameObject objValid = null;
+            UnityEngine.GameObject objToCheck = goInHierarchy;
+            string prefabPath = null;
+            do
             {
-                goPrefab = PrefabUtility.GetPrefabParent(goInHierarchy);
-                if (goPrefab != null)
+                var currPrefab = PrefabUtility.GetPrefabParent(goInHierarchy);
+                if (currPrefab == null)
                     break;
 
-                var t = goInHierarchy.transform.parent;
+                string currPath = AssetDatabase.GetAssetPath(currPrefab);
+                if (prefabPath == null)
+                    prefabPath = currPath;
+                if (currPath != prefabPath) // 已经到Root或者当前是嵌套prefab并且已经到达了上一层prefab
+                    break;
+                goPrefab = currPrefab;
+                objValid = objToCheck;
+
+                var t = objToCheck.transform.parent;
                 if (t != null)
-                    goInHierarchy = t.gameObject;
+                    objToCheck = t.gameObject;
                 else
                     break;
             }
+            while (true);
 
-            if (goPrefab != null)
+            if (objValid != null)
                 PrefabUtility.ReplacePrefab(goInHierarchy, goPrefab, ReplacePrefabOptions.ConnectToPrefab);
             else
                 Debug.LogFormat("<color=red>当前对象不属于Prefab, 请将其保存为 Prefab</color>");
         }
 
-        public static void ClearConsole()
-        {
-    #if UNITY_2017 || UNITY_2018
-            var logEntries = Type.GetType("UnityEditor.LogEntries,UnityEditor.dll");
-    #else
-            var logEntries = System.Type.GetType("UnityEditorInternal.LogEntries,UnityEditor.dll");
-    #endif
-            var clearMethod = logEntries.GetMethod("Clear", BindingFlags.Static | BindingFlags.Public);
-            clearMethod.Invoke(null, null);
-        }
+    //    public static void ClearConsole()
+    //    {
+    //#if UNITY_2017 || UNITY_2018
+    //        var logEntries = Type.GetType("UnityEditor.LogEntries,UnityEditor.dll");
+    //#else
+    //        var logEntries = System.Type.GetType("UnityEditorInternal.LogEntries,UnityEditor.dll");
+    //#endif
+    //        var clearMethod = logEntries.GetMethod("Clear", BindingFlags.Static | BindingFlags.Public);
+    //        clearMethod.Invoke(null, null);
+     //   }
     }
 
 }
