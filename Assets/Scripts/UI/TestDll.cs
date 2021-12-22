0d03bf928ac64a4bd9b9eb23bf6cb6e3109cd510
zhutaorun <zhutaorun@gmail.com>
2021-12-22 17:31:41 +0800

    DllTest

---
+++ Assets/Scripts/UI/TestDll.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class TestDll : MonoBehaviour
+{
+    [DllImport("DLLTest")]
+    public static extern int UnityDLLTest();
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("测试下C"+ UnityDLLTest());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
